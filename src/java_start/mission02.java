ab9adb50ded03714923ddad19570f7bda44097e4
gxnie <wlsdl7520@naver.com>
2024-10-31 21:35:33 +0900

    docs: 미션2

---
+++ src/java_start/mission02.java
@@ -0,0 +1,15 @@
+package java_start;
+
+public class mission02 {
+    public static void main(String[] args) {
+        // 초급 2번
+        String greeting = "Hello, SungbinClub!";
+        System.out.println(greeting);
+
+        // 중급 1번
+        final int DEFAULT_LIMIT = 50;
+        System.out.println(DEFAULT_LIMIT);
+//        DEFAULT_LIMIT = 10;
+//        java: cannot assign a value to final variable DEFAULT_LIMIT
+    }
+}
