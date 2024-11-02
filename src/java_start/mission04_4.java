9c8150835d21b870bef06eb6f875f83a214a26d6
gxnie <wlsdl7520@naver.com>
2024-11-02 15:45:10 +0900

    docs: 미션4_4

---
+++ src/java_start/mission04_4.java
@@ -0,0 +1,21 @@
+package java_start;
+
+import java.util.Scanner;
+
+public class mission04_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int weather = sc.nextInt();
+
+        switch (weather){
+            case 3, 4, 5:
+                System.out.println("봄");
+            case 6, 7, 8:
+                System.out.println("여름");
+            case 9,10,11:
+                System.out.println("가을");
+            case 12,1,2:
+                System.out.println("겨울");
+        }
+    }
+}
